5f732536b85ffa0767e40cc5fb4607aa68e60e81
Alice Lin <alice.2.lin@ucdenver.edu>
2023-04-07 10:51:38 -0600

    Added HIV_fit.py, updated HIV.py

---
+++ HIV_fit.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 10:46:38 2023
+
+@author: snowb
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.optimize import curve_fit
+
+# We fit experimental data to our viral load equation
